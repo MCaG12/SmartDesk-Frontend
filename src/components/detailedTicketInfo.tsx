a9cd3900e72b42145a69f25ab61ed6c84f6361f0
Murilo Carvalho <MuriloCarvalho2583@Outlook.com>
2026-07-26 19:56:50 -0300

    feature: adds support for the ticket comments fetching them

--- src/components/detailedTicketInfo.tsx
+++ src/components/detailedTicketInfo.tsx
@@ -1,13 +1,53 @@
+import { useEffect, useState } from "react";
 import type { i_Ticket } from "../interfaces/i_ticket";
+import type { i_ticketComment } from "../interfaces/i_ticketComment";
 
 interface i_DetailedTicketInfo
 {
     ticketInfo: i_Ticket,
-    setShowDetailedTicket: React.Dispatch<React.SetStateAction<boolean>>
+    setShowDetailedTicket: React.Dispatch<React.SetStateAction<i_ticketComment[] | undefined>>;
+}
+
+interface i_fetchTicketComments
+{
+    TicketId : number
+    setTicketComments:React.Dispatch<React.SetStateAction<i_ticketComment[] | undefined>>
+}
+
+
+async function FetchTicketComments({TicketId, setTicketComments}: i_fetchTicketComments)
+{
+    const url = "http://localhost:3000/TicketComment/fetch-ticket-comments/";
+
+    try
+    {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "ticketId": TicketId
+            })
+        });
+        const data = await response.json() as i_ticketComment[];
+        setTicketComments(data);
+    }
+    catch (error)
+    {
+        console.error("Error:", error);
+       
+    }
 }
 
 export default function DetailedTicketInfo({ticketInfo, setShowDetailedTicket}: i_DetailedTicketInfo)
 {
+    const [ticketComments, setTicketComments] = useState<i_ticketComment[]>()
+
+    useEffect(() => {
+        FetchTicketComments({ TicketId: ticketInfo.Id, setTicketComments });
+    }, [ticketInfo.Id]);
+
     return(
     <div className="detailed-ticket-overlay">
         <div className="detailed-ticket-modal">
@@ -39,6 +79,22 @@ export default function DetailedTicketInfo({ticketInfo, setShowDetailedTicket}: 
                 <label>Email Atendente</label>
                 <span>{ticketInfo.ticketAgent?.usuarEmail}</span>
                 </div>
+            </div>
+            <div className="detailed-ticket-comment-section">
+                {ticketComments?.map((comment) => {
+                    return (
+                        <div className="ticket-comment-item" key={comment.Id}>
+                            <div style={{display:"flex", flexDirection:"column"}}>
+                                <div style={{display:"Flex", flexDirection:"row", gap:"15%"}}>
+                                    <p className="ticket-comment-text">{comment.tickcomUser.usuarNome}</p>
+                                    <p className="ticket-comment-text">{comment.tickcomUser.usuarEmail}</p>
+                                </div>
+                                <p className="ticket-comment-text">{comment.tickcomComment}</p>   
+                            </div>
+                        </div>
+                    )
+                    })
+                }
             </div>
 
             </div>
--- src/interfaces/i_ticketComment.tsx
+++ src/interfaces/i_ticketComment.tsx
@@ -1,5 +1,21 @@
-export interface i_ticketComment 
-{
+export interface i_CommentTicket {
     Id: number;
-    tickcomComment: string
+    ticketTitle: string;
+    ticketDescription: string;
+    ticketDateOpen: string;
+    ticketDateClose: string | null;
+}
+
+export interface i_CommentTicketUserInfo {
+    Id: number;
+    usuarNome: string;
+    usuarEmail: string;
+    usuarSenha: string;
+}
+
+export interface i_ticketComment {
+    Id: number;
+    tickcomComment: string;
+    tickcomTicket: i_CommentTicket;
+    tickcomUser: i_CommentTicketUserInfo;
 }
